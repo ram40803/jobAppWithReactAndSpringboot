247332c15045063642a1152477777eda50649fa5
Ram Patidar <rampatidar40803@gmail.com>
2025-06-24 11:21:07 +0530

    add more funcnality to jobApp

--- jobAppUIWithReact/src/components/AllPosts.jsx
+++ jobAppUIWithReact/src/components/AllPosts.jsx
@@ -1,83 +1,112 @@
 import React from 'react'
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import SearchIcon from '@mui/icons-material/Search';
 import {
-    Card,
-    Grid,
-    Typography,
-  } from "@mui/material";
-  import axios from "axios";
-  import { useEffect, useState } from "react";
+  Box,
+  // SearchIcon,
+  Card,
+  Grid,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
-    const [post, setPost] = useState(null);
-    const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+  const [post, setPost] = useState(null);
+  const navigate = useNavigate();
 
-const handleEdit = (id) => {
-  navigate("/edit",{state:{id}});
-}
+  const handleEdit = (id) => {
+    navigate("/edit", { state: { id } });
+  }
 
-    useEffect(() => {
-        const fetchInitialPosts = async () => {
-            const response = await axios.get(`http://localhost:8080/jobPosts`);
-            setPost(response.data);
-            console.log(post);
-        }
-         fetchInitialPosts();
-      }, []);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const respnse = await axios.get(`http://localhost:8080/jobPosts/keyword/${query}`);
+      setPost(respnse.data);
+    }
 
-      const handleDelete = (id) => {
-        async function deletePost() {
-          await axios.delete(`http://localhost:8080/jobPost/${id}`);
-          console.log("Delete")
-      }
-      deletePost();
-      window.location.reload();
-      }
+    const fetchInitialPosts = async () => {
+      const response = await axios.get(`http://localhost:8080/jobPosts`);
+      setPost(response.data);
+      console.log(post);
+    }
+
+    // fetchInitialPosts();
+    if (query.length === 0) fetchInitialPosts();
+    if (query.length >= 2) fetchPosts();
+  }, [query]);
+
+  const handleDelete = (id) => {
+    async function deletePost() {
+      await axios.delete(`http://localhost:8080/jobPost/${id}`);
+      console.log("Delete")
+    }
+    deletePost();
+    window.location.reload();
+  }
 
   return (
     <>
       <Grid container spacing={2} sx={{ margin: "2%" }}>
-      <Grid item xs={12} sx={12} md={12} lg={12}>
+        <Grid item xs={12} sx={12} md={12} lg={12}>
+        </Grid>
+        <Box width={"100%"}>
+          <TextField
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+            placeholder='Search...'
+            sx={{ width: "75%", padding: "2% auto" }}
+            fullWidth
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </Box>
+        {post &&
+          post.map((p) => {
+            return (
+              <Grid key={p.id} item xs={12} md={6} lg={4}>
+                <Card sx={{ padding: "3%", overflow: "hidden", width: "84%", backgroundColor: "#ADD8E6" }}>
+                  <Typography
+                    variant="h5"
+                    sx={{ fontSize: "2rem", fontWeight: "600", fontFamily: "sans-serif" }}
+                  >
+                    {p.postProfile}
+                  </Typography>
+                  <Typography sx={{ color: "#585858", marginTop: "2%", fontFamily: "cursive" }} variant="body" >
+                    Description: {p.postDesc}
+                  </Typography>
+                  <br />
+                  <br />
+                  <Typography variant="h6" sx={{ fontFamily: "unset", fontSize: "400" }}>
+                    Experience: {p.reqExperience} years
+                  </Typography>
+                  <Typography sx={{ fontFamily: "serif", fontSize: "400" }} gutterBottom variant="body">Skills : </Typography>
+                  {p.postTechStack.map((s, i) => {
+                    return (
+                      <Typography variant="body" gutterBottom key={i}>
+                        {s} .
+                        {` `}
+                      </Typography>
+                    );
+                  })}
+                  <DeleteIcon onClick={() => handleDelete(p.postId)} />
+                  <EditIcon onClick={() => handleEdit(p.postId)} />
+                </Card>
+              </Grid>
+            );
+          })}
       </Grid>
-      {post &&
-        post.map((p) => {
-          return (
-            <Grid key={p.id} item xs={12} md={6} lg={4}>
-              <Card sx={{ padding: "3%", overflow: "hidden", width: "84%", backgroundColor:"#ADD8E6" }}>
-                <Typography        
-                  variant="h5"
-                  sx={{ fontSize: "2rem", fontWeight: "600", fontFamily:"sans-serif" }}
-                >
-             {p.postProfile}
-                </Typography>
-                <Typography  sx={{ color: "#585858", marginTop:"2%", fontFamily:"cursive" }} variant="body" >
-                  Description: {p.postDesc}
-                </Typography>
-                <br />
-                <br />
-                <Typography variant="h6" sx={{ fontFamily:"unset", fontSize:"400"}}>
-                  Experience: {p.reqExperience} years
-                </Typography>
-                <Typography sx={{fontFamily:"serif",fontSize:"400"}} gutterBottom  variant="body">Skills : </Typography>
-                {p.postTechStack.map((s, i) => {
-                  return (
-                    <Typography variant="body" gutterBottom key={i}>
-                      {s} .
-                      {` `}
-                    </Typography>
-                  );
-                })}
-               <DeleteIcon onClick={() => handleDelete(p.postId)} />
-                <EditIcon onClick={() => handleEdit(p.postId)} />
-              </Card>
-            </Grid>
-          );
-        })}
-    </Grid>
     </>
- 
+
   )
 }
 
